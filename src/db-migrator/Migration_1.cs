2f35cf3fb7ade7f14ce34423eaa1d395559f22c3
Kurt Johnson <kujotx@gmail.com>
2017-06-19 21:43:50 -0500

    Add the database migration project

---
+++ src/db-migrator/Migration_1.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_migrator
+{
+    /// <summary>
+    /// First migration for creating the database
+    /// </summary>
+    [Migration(1)]
+    public class Migration_1 : FluentMigrator.ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
