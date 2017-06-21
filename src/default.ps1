1bab481b7e98a2dae12511eb5c222c7e2f618718
Kurt Johnson <kujotx@gmail.com>
2017-06-21 10:04:45 -0500

    Add a Psake Task to migrate the database

--- src/default.ps1
+++ src/default.ps1
@@ -1,6 +1,6 @@
 Include '.\properties.ps1'
 
-Task Default -Depends Build,DbDropAndSetup -Description "Cleans, Builds the application"
+Task Default -Depends Build,DbDropAndSetup,DbMigrate -Description "Cleans, Builds the application"
 
 Task Clean -Description "Clean the solution" {
 	Exec {
@@ -14,17 +14,18 @@ Task Build -Depends Clean -Description "Builds the solution file" {
 	}
 }
 
-Task dbdropandsetup -description "Drops and re-creates the database" {
+Task DbDropAndSetup -description "Drops and re-creates the database" {
 	exec {
 		&sqlcmd.exe -i $init_db -e -S "$dbserver" -d "master" -v databasename="$($dbname)" -v dbusername="$($dbusername)" -v dbpassword="$($dbpassword)" -t 60 -l 60 | out-null
 	}
 }
 
-# Task DbMigrate -Description "Migrates the database to the latest version" {
-	# Exec {
-		# $migrate_cfg = [xml](Get-Content .\db-migrator\packages.config)
-		# $runner=$migrate_cfg.SelectSingleNode("//packages/package[@id='FluentMigrator']")
-		# $rver=$runner.GetAttribute("version")
-		# $migrate_exe = ".\packages\FluentMigrator.$(rver)\tools\migrate.exe" -a ".\db-migrator\bin\$Configuration\db-migrator.dll" -c="Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=$(dbname);Data Source=$(dbserver)"
-	# }
-# } 
+ Task DbMigrate -Description "Migrates the database to the latest version" {
+	 Exec {
+		 $migrate_cfg = [xml](Get-Content .\db-migrator\packages.config)
+		 $runner=$migrate_cfg.SelectSingleNode("//packages/package[@id='FluentMigrator']")
+		 $rver=$runner.GetAttribute("version")
+		 $migrate_exe = ".\packages\FluentMigrator.$rver\tools\migrate.exe" 
+		 & $migrate_exe -a ".\db-migrator\bin\$Configuration\db-migrator.dll" -db="SqlServer" -conn="Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=$dbname;Data Source=$dbserver;"
+	 }
+ } 
